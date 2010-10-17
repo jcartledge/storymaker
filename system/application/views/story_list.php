2c8b3d525f5d9204b8463c3496a0c3d2571c6f9c
James Cartledge <jcartledge@gmail.com>
2010-10-17 17:21:45 +1100

    cleanup

--- system/application/views/story_list.php
+++ system/application/views/story_list.php
@@ -1,7 +1,7 @@
-      <ul><?php foreach ($stories as $story) { ?>
-        <li>
-          <p><?php echo anchor('/story/view/' . $story->id, $story->title); ?> [<?php echo $story->username; ?>]</p>
-          <?php echo $story->description; ?>
-        </li>
-      <?php } ?></ul>
+<ul><?php foreach ($stories as $story) { ?>
+  <li>
+    <p><?php echo anchor('/story/view/' . $story->id, $story->title); ?> [<?php echo $story->username; ?>]</p>
+    <?php echo $story->description; ?>
+  </li>
+<?php } ?></ul>
 
--- system/application/views/story_search_form.php
+++ system/application/views/story_search_form.php
@@ -1,4 +1,4 @@
-    <?php echo form_open('/story/search', array('method' => 'get')); ?>
-      Search: <?php echo form_input('q', isset($search) ? $search : ''); ?>
-      <?php echo form_submit('search', 'Go'); ?>
-    <?php echo form_close(); ?>
+<?php echo form_open('/story/search', array('method' => 'get')); ?>
+  Search: <?php echo form_input('q', isset($search) ? $search : ''); ?>
+  <?php echo form_submit('search', 'Go'); ?>
+<?php echo form_close(); ?>
