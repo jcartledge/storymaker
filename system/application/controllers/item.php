8f802168390c9d69a279aa484ad75ce041bae5a9
James Cartledge <jcartledge@gmail.com>
2010-11-08 20:08:54 +1100

    only permit owner to set story/item pos

--- system/application/controllers/item.php
+++ system/application/controllers/item.php
@@ -1,8 +1,16 @@
 <?php
 
 class Item extends Controller {
-  function position() {
+  function __construct() {
+    parent::__construct();
     $this->load->model('Story_model', '', TRUE);
-    $this->Story_model->update_item_position($_GET);
+		$this->load->library('tank_auth');
+  }
+
+  function position() {
+    $story = $this->Story_model->load($_GET['story_id']);
+    if($story->username == $this->tank_auth->get_username()) {
+      $this->Story_model->update_item_position($_GET);
+    }
   }
 }
