455b38cf6f77f88038c05a59ef77cbb8764bfce3
James Cartledge <jcartledge@gmail.com>
2010-11-16 21:54:01 +1100

    pager for item search fix

--- system/application/views/item/pager.php
+++ system/application/views/item/pager.php
@@ -5,7 +5,8 @@ if(!$page) $page = 1;
 if(!is_callable('pager_link')) {
   function pager_link($page) {
     $url = parse_url($_SERVER['REQUEST_URI']);
-    parse_str($url['query'], $query);
+    $query = array();
+    if(isset($url['query'])) parse_str($url['query'], $query);
     $query['page'] = $page;
     $url['query'] = http_build_query($query);
     return sprintf('%s?%s', $url['path'], $url['query']);
@@ -16,7 +17,7 @@ if(!is_callable('pager_link')) {
   <?php if($page > 1) { ?><a class="prev" href="<?php echo pager_link($page - 1); ?>">Previous</a><?php } ?>
   <?php if($page < $num_pages) { ?><a class="next" href="<?php echo pager_link($page + 1); ?>">Next</a><?php } ?>
   <p>
-    Showing <?php $first = 1 + ($page_size * ($page - 1)); echo $first; ?> to <?php echo $first + count($items); ?>
+    Showing <?php $first = 1 + ($page_size * ($page - 1)); echo $first; ?> to <?php echo $first + count($items) - 1; ?>
     of <?php echo $num_items; ?> unused
     item<?php if($num_items != 1) echo 's'; ?><?php if($item_search) echo " matching <em>{$item_search}</em>"; ?>.
   </p>
