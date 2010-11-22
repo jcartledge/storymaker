40736f7482ffd012db7a7ba5b3885cfeb2dfa79c
James Cartledge <jcartledge@gmail.com>
2010-11-23 02:09:41 +1100

    remove unimplemented actions

--- system/application/views/manage/index.php
+++ system/application/views/manage/index.php
@@ -1,12 +1,12 @@
 <div class="items">
   <h2>My items</h2>
   <a class="add-item" href="/item/add"><?php echo icon('add'); ?> Add an item</a>
-  <?php echo $this->load->view('item/manage-list', array('items' => $items, 'actions' => array('edit', 'delete'))); ?>
+  <?php echo $this->load->view('item/manage-list', array('items' => $items, 'actions' => array('delete'))); ?>
 </div>
 <div class="stories">
   <h2>My stories</h2>
   <a class="add-story" href="/story/add"><?php echo icon('add'); ?> Add a story</a>
-  <?php echo $this->load->view('story/manage-list', array('stories' => $stories, 'actions' => array('edit', 'delete'))); ?>
+  <?php echo $this->load->view('story/manage-list', array('stories' => $stories, 'actions' => array('edit'))); ?>
 </div>
 <script>
 $(function(){
