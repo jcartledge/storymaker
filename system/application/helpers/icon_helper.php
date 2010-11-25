bce066f8dad77c572f834497d3c63235f412b470
James Cartledge <jcartledge@gmail.com>
2010-11-25 20:57:42 +1100

    typo

--- system/application/helpers/icon_helper.php
+++ system/application/helpers/icon_helper.php
@@ -4,6 +4,6 @@
  * shorthand for a silk icon image tag
  */
 function icon($name) {
-  $url = site_url(sprintf('images/silk/%s.png, $name'));
+  $url = site_url(sprintf('images/silk/%s.png', $name));
   return sprintf('<img class="icon %s" src="%s">', $name, $url  );
 }
