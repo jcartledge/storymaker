16961e9aa8ca9c355edb9e0527c2830c1c2b473e
James Cartledge <jcartledge@gmail.com>
2010-11-28 17:04:31 +1100

    add story tweaks

--- system/application/views/story/add.php
+++ system/application/views/story/add.php
@@ -1,10 +1,11 @@
 <form class="add-story" action="<?php echo site_url('story/add'); ?>" method="post">
   <h2>Build a story</h2>
-  <label for="story-title">Give your story a name</label>
+  <p>Required fields are marked with a <span class="required">*</span>.</p>
+  <label for="story-title">Give your story a name <span class="required">*</span></label>
   <?php echo form_error('story-title'); ?>
   <input class="long" name="story-title" value="<?php echo set_value('story-title'); ?>">
   <br>
-  <label for="story-description">Please provide a brief description of the story</label>
+  <label for="story-description">Please provide a brief description of the story <span class="required">*</span></label>
   <?php echo form_error('story-description'); ?>
   <textarea name="story-description"><?php echo set_value('story-description'); ?></textarea>
   <br>
