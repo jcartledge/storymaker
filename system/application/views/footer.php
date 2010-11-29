66f7b5c7f143e7d8f1a581916da0fc1a237e0b4b
James Cartledge <jcartledge@gmail.com>
2010-11-29 21:22:50 +1100

    remove footer links

--- system/application/views/footer.php
+++ system/application/views/footer.php
@@ -3,6 +3,4 @@
   <a href="<?php echo site_url(); ?>">Home</a> |
   <a href="<?php echo site_url('page/disclaimer'); ?>">Disclaimer</a> |
   <a href="<?php echo site_url('page/privacy'); ?>">Privacy statement</a> |
-  <a href="<?php echo site_url('page/copyright'); ?>">Copyright</a> |
-  <a href="<?php echo site_url('page/thanks'); ?>">Thanks</a>
 </div>
